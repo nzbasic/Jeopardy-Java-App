d27ffefab1ac0c02942a103dc9e43cc867e9036f
james <jamescoppard024@gmail.com>
2020-08-29 22:46:57 +1200

    Fix constraints, empty boxes

--- Jeopardy/Controller.java
+++ Jeopardy/Controller.java
@@ -10,6 +10,7 @@ import java.io.FileInputStream;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
@@ -20,26 +21,28 @@ import java.nio.file.Paths;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
-import javafx.scene.layout.Pane;
+
 import javafx.scene.control.Button;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import java.io.IOException;
+import javafx.scene.layout.Priority;
 
 public class Controller {
 
     private GridPane grid = new GridPane();
 
     private void addPaneText(String text, int i, int j) {
-        Label label = new Label(text);
+        Label label = new Label(text.toUpperCase());
         label.setTextAlignment(TextAlignment.CENTER);
-        label.setFont(new Font(18));
+        label.setFont(new Font(14));
         label.setTextFill(Color.WHITE);
         label.setAlignment(Pos.CENTER);
+        label.setWrapText(true);
 
         AnchorPane canvas = new AnchorPane();
         canvas.setPrefSize(200,100);
-        canvas.setStyle("-fx-background-color: #0F4C75");
+        canvas.setStyle("-fx-background-color: #0F4C75; -fx-border-color: black; -fx-border-width: 3 3 3 3;");
         canvas.getChildren().addAll(label);
 
         AnchorPane.setTopAnchor(label, 5.0);
@@ -48,22 +51,21 @@ public class Controller {
         AnchorPane.setRightAnchor(label, 5.0);
 
         GridPane.setConstraints(canvas, i, j);
-        grid.getColumnConstraints().add(new ColumnConstraints(100));
         grid.getChildren().addAll(canvas);
     }
 
     private void addButtonText(String text, int i, int j, EventHandler<ActionEvent> onClick) {
         Button button = new Button(text);
         button.setTextAlignment(TextAlignment.CENTER);
-        button.setFont(new Font(18));
+        button.setFont(new Font(25));
         button.setTextFill(Color.YELLOW);
 
         button.setPrefSize(200,100);
+        button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
         button.setOnAction(onClick);
 
         GridPane.setConstraints(button, i, j);
-        grid.getColumnConstraints().add(new ColumnConstraints(100));
         grid.getChildren().addAll(button);
     }
 
@@ -91,19 +93,22 @@ public class Controller {
                 addButtonText(question.getPrize(), i, j, new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
-
                     }
                 });
 
                 j++;
-                if (j == 5) {
+                if (j == 6) {
                     break;
                 }
             }
 
             if (j < 5) {
                 for (int k=j; k<6; k++) {
-                    
+                    addButtonText("", i, k, new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent event) {
+                        }
+                    });
                 }
             }
 
@@ -115,22 +120,27 @@ public class Controller {
 
         if (i < 6) {
             for (int k=i; k<6; k++) {
-                
-                
+                addPaneText("", k, 0);
+                for (int l=1; l<6; l++) {
+                    addButtonText("", k, l, new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent event) {
+                        }
+                    });
+                }
             }
         }
 
         AnchorPane root = new AnchorPane(grid);
-        root.setStyle("-fx-background-color: #0F4C75");
-        root.setPrefSize(600,400);
-        AnchorPane.setTopAnchor(grid, 10.0);
-        AnchorPane.setBottomAnchor(grid, 10.0);
-        AnchorPane.setLeftAnchor(grid, 10.0);
-        AnchorPane.setRightAnchor(grid, 10.0);
+        root.setPrefSize(1000,600);
+        AnchorPane.setTopAnchor(grid, 0.0);
+        AnchorPane.setBottomAnchor(grid, 0.0);
+        AnchorPane.setLeftAnchor(grid, 0.0);
+        AnchorPane.setRightAnchor(grid, 0.0);
 
         root.setStyle("-fx-background-color: #121212");
 
-        Scene scene2 = new Scene(root, 400, 400);
+        Scene scene2 = new Scene(root, 750, 400);
         scene2.getStylesheets().add("./questions.css");
 
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
